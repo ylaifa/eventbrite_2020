42e461692040f79bb712f1b77541d9bb83018d2f
Yousri <laifayousri@gmail.com>
2020-02-18 19:29:10 +0100

    profils table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_180547) do
+ActiveRecord::Schema.define(version: 2020_02_18_182219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 2020_02_18_180547) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "admin"
+  end
+
+  create_table "profils", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profils_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
