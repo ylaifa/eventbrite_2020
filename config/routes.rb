1acfee8bd074ebb88820d5b61c9401f9aafb107a
Yousri <laifayousri@gmail.com>
2020-02-18 17:02:12 +0100

    events_controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/show'
+  get 'events/new'
+  get 'events/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
