480ab92c4889e6f76f7f62f176aa7e923fe7f8a8
Yousri <laifayousri@gmail.com>
2020-02-18 19:12:26 +0100

    add admin to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_155406) do
+ActiveRecord::Schema.define(version: 2020_02_18_180547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_155406) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "admin"
   end
 
   create_table "users", force: :cascade do |t|
