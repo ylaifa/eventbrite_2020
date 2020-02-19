ead23624d457f05baa28198a7984d49acb9f23a4
Yousri <laifayousri@gmail.com>
2020-02-19 18:06:18 +0100

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Event.destroy_all
+
+10.times do
+  User.create!(
+    email: Faker::Name.first_name.downcase + "@yopmail.com",
+    password: "azerty"
+  )
+end
+
+
+multiple_of_five = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+a=1
+20.times do
+  Event.create!(
+    start_date: Faker::Time.between(from: DateTime.now, to: DateTime.now + 30),
+    duration: multiple_of_five.sample,
+    title: "Fake Event " + a.to_s,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Number.between(from: 20, to: 1000),
+    location: Faker::Address.city,
+    admin: User.all.sample
+  )
+a+=1
+end
