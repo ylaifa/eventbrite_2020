9f68b9b124ade5391ecd97c0f0be4b8b5fa8631b
Yousri <laifayousri@gmail.com>
2020-02-19 13:01:15 +0100

    edit User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :profil 
+  has_one :profile
   has_many :attendances
   has_many :events, through: :attendances
 end
