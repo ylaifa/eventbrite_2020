e259b5a419ffca050b10126316e73db083b34e74
Yousri <laifayousri@gmail.com>
2020-02-21 17:12:50 +0100

    add welcome_event_email after_create Attendance

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,19 @@ class UserMailer < ApplicationMailer
       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
       mail(to: @user.email, subject: 'Bienvenue chez nous !') 
     end
+
+    def welcome_event_email(event, user)
+      #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+      @user = user 
+
+      #on récupère l'instance event pour ensuite pouvoir la passer à la view en @event
+
+      @event = event
+  
+      #on définit une variable @url qu'on utilisera dans la view d’e-mail
+      @url  = 'http://monsite.fr/login' 
+  
+      # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+      mail(to: @user.email, subject: "Inscription validée !") 
+    end
   end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,10 @@
 class Attendance < ApplicationRecord
+    after_create_commit :welcome_event_send
+ 
     belongs_to :user
     belongs_to :event
+
+    def welcome_event_send
+        UserMailer.welcome_event_email(self.event, self.user).deliver_now
+    end 
 end
