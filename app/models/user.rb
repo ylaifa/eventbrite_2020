49d39f17df98523355cbdfe28bf7ee5d1c925791
Yousri <laifayousri@gmail.com>
2020-02-21 14:37:17 +0100

    welcome_email created

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   after_create :init_profile
+  after_create :welcome_send
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -12,5 +13,9 @@ class User < ApplicationRecord
 
   def init_profile
     self.create_profile!
+  end
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
   end
 end
