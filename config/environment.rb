aa372a92a8da37297845d4453a1b5f26d20bc9da
Yousri <laifayousri@gmail.com>
2020-02-21 16:06:14 +0100

    edit SMTP to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['MAILJET_LOGIN'],
-    :password => ENV['MAILJET_PWD'],
-    :domain => 'monsite.fr',
-    :address => 'in-v3.mailjet.com',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  user_name: ENV['SENDGRID_LOGIN'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'monsite.fr',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
