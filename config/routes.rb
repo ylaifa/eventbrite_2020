afc5f93cc5ece753fb78c880d1fd911d10388a5f
Yousri <laifayousri@gmail.com>
2020-02-22 18:26:41 +0100

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :profiles
   resources :events
   resources :attendances
+  
   post "events/subscribe/:id", to: "subscribes#create", as: "subscribes"
+  get "events/subscribe/:id", to: "subscribes#free_subscribe", as: "free_subscribe" 
  
   root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
