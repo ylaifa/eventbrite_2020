147f1c1fffbeaf6e8299679213621ce6627637a5
Yousri <laifayousri@gmail.com>
2020-02-22 21:27:26 +0100

    edit model Event add association

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,6 +15,7 @@ class Event < ApplicationRecord
     validates :start_date, presence: true
     validate :date_passed
 
+    has_one_attached :event_picture
     has_many :attendances
     has_many :users, through: :attendances
 
