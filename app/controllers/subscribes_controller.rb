f6ac96008a424424dcd96eceba0f4caf20f546e3
Yousri <laifayousri@gmail.com>
2020-02-22 12:34:17 +0100

    subscribes_controller created

---
+++ app/controllers/subscribes_controller.rb
@@ -0,0 +1,31 @@
+class SubscribesController < ApplicationController
+    def new
+    end
+
+    def create  
+        @event = Event.find(params[:id])
+    
+        @amount = @event.price * 100
+    
+        customer = Stripe::Customer.create({
+          email: params[:stripeEmail],
+          source: params[:stripeToken],
+        })
+      
+        charge = Stripe::Charge.create({
+          customer: customer.id,
+          amount: @amount,
+          description: 'Eventbite payment',
+          currency: 'eur',
+        })
+    
+        # Création d'une participation d'un user à l'évènement
+        @attendance = Attendance.create(stripe_customer_id: customer.id, user: current_user, event: @event)
+        
+        redirect_to event_path(@event.id)
+      
+        rescue Stripe::CardError => e
+            flash[:error] = e.message
+            redirect_to @event
+    end
+end
