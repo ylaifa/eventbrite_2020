17bcec1b4d9dd8db6cf145cf34b7c3d5bb30e14f
Yousri <laifayousri@gmail.com>
2020-02-22 12:35:18 +0100

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users 
   resources :profiles
   resources :events
-  resources :charges
- 
 
+  post "events/subscribe/:id", to: "subscribes#create", as: "subscribes"
+ 
   root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
