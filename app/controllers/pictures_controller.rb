dbcfa4e613b0b22d48ada9d4fca649005a523e9d
Yousri <laifayousri@gmail.com>
2020-02-23 00:09:34 +0100

    edit pictures_controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,7 @@
 class PicturesController < ApplicationController
   def create
+    @event = Event.find(params[:event_id])
+    @event.picture.attach(params[:picture])
+    redirect_to(event_path(@event))
   end
 end
