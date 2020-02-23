c0322b6b1e765697be973bd5ee9e721319fdcd53
Yousri <laifayousri@gmail.com>
2020-02-23 22:55:53 +0100

    edit avatars_controller

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,4 +1,7 @@
 class AvatarsController < ApplicationController
   def create
+    @profile = Profile.find(params[:profile_id])
+    @profile.avatar.attach(params[:avatar])
+    redirect_to(profile_path(@profile))
   end
 end
