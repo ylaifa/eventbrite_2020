922085d3a45619a99ab0a4f7f3f45bb922981217
Yousri <laifayousri@gmail.com>
2020-02-24 17:46:12 +0100

    avatars_controller deleted

--- app/controllers/avatars_controller.rb
+++
@@ -1,7 +0,0 @@
-class AvatarsController < ApplicationController
-  def create
-    @profile = Profile.find(params[:profile_id])
-    @profile.avatar.attach(params[:avatar])
-    redirect_to(profile_path(@profile))
-  end
-end
