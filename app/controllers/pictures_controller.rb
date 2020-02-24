bbe6e8ff192eb7bc2cfb57748448e9c1775f0bd2
Yousri <laifayousri@gmail.com>
2020-02-24 17:46:56 +0100

    pictures_controller deleted

--- app/controllers/pictures_controller.rb
+++
@@ -1,7 +0,0 @@
-class PicturesController < ApplicationController
-  def create
-    @event = Event.find(params[:event_id])
-    @event.picture.attach(params[:picture])
-    redirect_to(event_path(@event))
-  end
-end
